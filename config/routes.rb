8cc2875dec8d7bc2b027db54e3099ced2b30571c
Arkadiusz Hiler <arek.l1@gmail.com>
2012-03-01 23:19:59 +0100

    added routs for hook

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 if defined? map
   map.resources :public_keys, :controller => 'gitolite_public_keys', :path_prefix => 'my'
+  map.connect  'gitolite_hook', :controller => 'gitolite_hook', :action => 'index'
 else
   ActionController::Routing::Routes.draw do |map|
     map.resources :public_keys, :controller => 'gitolite_public_keys', :path_prefix => 'my'
+    map.connect  'gitolite_hook', :controller => 'gitolite_hook', :action => 'index'
   end
 end
