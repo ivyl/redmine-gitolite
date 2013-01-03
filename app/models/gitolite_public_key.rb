a296871f2bee2078b52472a85524392c087ea731
Rohit Sankaran <rohit.sankaran@gmail.com>
2013-01-03 10:07:23 -0600

    Added SSH Key validation when a key is added

--- app/models/gitolite_public_key.rb
+++ app/models/gitolite_public_key.rb
@@ -1,3 +1,5 @@
+require 'open3'
+
 class GitolitePublicKey < ActiveRecord::Base
   STATUS_ACTIVE = 1
   STATUS_LOCKED = 0
@@ -10,7 +12,7 @@ class GitolitePublicKey < ActiveRecord::Base
   named_scope :active, {:conditions => {:active => GitolitePublicKey::STATUS_ACTIVE}}
   named_scope :inactive, {:conditions => {:active => GitolitePublicKey::STATUS_LOCKED}}
   
-  validate :has_not_been_changed
+  validate :has_not_been_changed, :is_valid_key
   
   before_validation :set_identifier
   
@@ -19,6 +21,37 @@ class GitolitePublicKey < ActiveRecord::Base
       %w(identifier key user_id).each do |attribute|
         errors.add(attribute, 'may not be changed') unless changes[attribute].blank?
       end
+    end
+  end
+
+  def is_valid_key
+    valid = check_key(key)
+    if !valid
+      begin
+        key = "#{key}\n"
+        valid = check_key(key)
+        if !valid
+          errors[:base] = "invalid key"
+        else
+          self.key = key
+        end
+      rescue
+        errors[:key] = "invalid key"
+      end
+    end
+  end
+
+  def check_key(key)
+    File.open('/tmp/check_key', 'w') {|f| f.write(key)}
+    stdin, stdout, stderr = Open3.popen3('ssh-keygen -l -f /tmp/check_key')
+    data = stdout.gets()
+    hash_regex = /([0-9a-f][0-9a-f](:[0-9a-f][0-9a-f])+)/
+    matching = hash_regex.match data
+    File.delete('/tmp/check_key')
+    if matching == nil
+      false
+    else
+      true
     end
   end
   
